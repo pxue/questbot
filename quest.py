7168253c21ea71727bdee4620fb17e6fd1bb3483
Paul Xue <xuepaul.xp@gmail.com>
2013-04-23 21:58:42 -0400

    added change checking, only sends email on change, requires sqlite3

--- quest.py
+++ quest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import re
+import sqlite3
 import mechanize
 import smtplib
 from email.mime.text import MIMEText
@@ -8,20 +9,12 @@ from collections import OrderedDict
 from bs4 import BeautifulSoup
 import settings
 
-
-def catchall(func):
-    def dec(*args):
-        try:
-            func(*args)
-        except Exception, e:
-            print "Ops, something went wrong! %d: %s" % (e.code, e.msg)
-            raise e
-    return dec
-
+conn = None
+br = None
 
 # Get quest pages and parse the result
-@catchall
-def quest(br):
+#@catchall
+def quest():
 
     # try to log into quest
     br.open(settings.LOGIN_URL)
@@ -41,7 +34,7 @@ def quest(br):
 
     # select the latest term, you can change the number here
     # from 1 to i for which ever term you want
-    br["SSR_DUMMY_RECV1$sels$0"] = ['0']
+    br["SSR_DUMMY_RECV1$sels$0"] = ['2']
 
     # do not modify any thing below here
     br["DERIVED_SSTSNAV_SSTS_MAIN_GOTO$22$"] = ['9999']
@@ -64,7 +57,36 @@ def quest(br):
 
     return True
 
-@catchall
+
+def check_changes(course_num, course_grade):
+    
+    c = conn.cursor()
+    # check if grades table exist, create if do not
+    c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='grades'")
+    if not c.fetchone():
+        c.execute("CREATE TABLE grades (course text, grade real)")
+        conn.commit()
+
+    # table exists, check if grade changed
+    print "checking", course_num
+    c.execute("SELECT grade FROM grades WHERE course=?", [course_num])
+    grade = c.fetchone()
+    if not grade:
+        # grade changed: insert into table and return true
+        c.execute("INSERT INTO grades VALUES (?, ?)", [course_num, course_grade])
+        conn.commit()
+        return True
+    elif str(grade[0]) != str(course_grade):
+
+        # grades already in there, but changed
+        c.execute("UPDATE grades SET grade=? WHERE course=?", [course_grade, course_num])
+        conn.commit()
+        return True
+
+    return False
+
+
+#@catchall
 def parse_data(data):
     if data is None: return "Empty data"
 
@@ -74,7 +96,7 @@ def parse_data(data):
     # get COURSES and Send email
     courses = soup.find_all('tr', id=re.compile("trTERM_CLASSES"))
 
-    def smtp_service(courses):
+    def smtp_service(courses, has_changes=False):
 
         # build html message
         msg = MIMEMultipart('alternative')
@@ -100,8 +122,32 @@ def parse_data(data):
             course_grade = course.find('span',
                     id=re.compile("CRSE_GRADE_OFF")).string.strip()
 
+
+            course_change = False
+            if course_grade != "":
+                course_change = check_changes(course_num, float(course_grade))
+
+            has_changes = course_change or has_changes
+
+            # bold and plus size if changed
+            # assign different color depending on grades:
+            #  < 50 is RED
+            #  50 <= x <= 70 is ORANGE 
+            #  70 > is GREEN
+            color = ""
+            if course_grade != "":
+                color = "orange"
+                if float(course_grade) < 50:
+                    color = "red"
+                elif float(course_grade) > 70:
+                    color = "green"
+
+            if course_change:
+                html += "<tr style='font-weight: bold; font-size: 18px; color: %s;'>" % color
+            else:
+                html += "<tr style='color: %s'>" % color
+
             html += """
-                        <tr>
                             <td>%s</td>
                             <td>%s</td>
                         </tr>
@@ -113,15 +159,16 @@ def parse_data(data):
             </html>
         """
 
-        msg.attach(MIMEText(html, 'html'))
+        if has_changes:
+            msg.attach(MIMEText(html, 'html'))
 
-        s = smtplib.SMTP(settings.SMTP["server"], settings.SMTP["port"])
-        s.ehlo()
-        s.starttls()
-        s.login(settings.SMTP["username"], settings.SMTP["password"])
+            s = smtplib.SMTP(settings.SMTP["server"], settings.SMTP["port"])
+            s.ehlo()
+            s.starttls()
+            s.login(settings.SMTP["username"], settings.SMTP["password"])
 
-        s.sendmail(settings.ME, settings.YOU, msg.as_string())
-        s.quit()
+            s.sendmail(settings.ME, settings.YOU, msg.as_string())
+            s.quit()
 
     smtp_service(courses)
 
@@ -129,11 +176,16 @@ def parse_data(data):
 
 
 if __name__ == "__main__":
+    # initialize mechanize browser
     br = mechanize.Browser()
 
-    # login and parse quest pages
-    response = quest(br)
+    # initialize sqlite storage
+    conn = sqlite3.connect("grades.db")
 
+    # login and parse quest pages
+    response = quest()
+
+    conn.close()
     print "Your daily digest has completed."
 
 
